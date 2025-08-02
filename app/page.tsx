8c9cee06083b61d0e2cfbf7786fa8e70a82becf1
Jens Bodal <jensbodal@gmail.com>
2025-08-02 13:50:20 -0700

    Remove scroll indicator arrow from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -76,12 +76,6 @@ export default function HomePage() {
           </div>
         </div>
 
-        {/* Scroll indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-gray-500 animate-bounce">
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
-          </svg>
-        </div>
       </section>
 
       {/* Additional sections can be added here */}
